eb58e179e1d2a99319bff41e3f15c34d7f9da593
AlexLeopold <71559312+AlexLeopold@users.noreply.github.com>
2021-03-24 11:47:14 -0400

    HW_4 done

---
+++ HW_4/HW_4.js
@@ -0,0 +1,20 @@
+let consumption = [];
+let days = 7;
+
+for (i = 1; i <= days; i++) {
+   consumption.push({
+      meat: Math.floor(Math.random()* 40),
+      rum: Math.floor(Math.random()* 40),
+      day: i,
+   })
+}
+
+let maxMeatDay = consumption.reduce((acc, curr) => acc.meat > curr.meat ? acc : curr);
+let maxRumDay = consumption.reduce((acc, curr) => acc.rum > curr.rum ? acc : curr);
+
+console.log(consumption);
+
+console.log(`На ${maxMeatDay.day} было сьеденно больше всего мяса, 
+На ${maxRumDay.day} было выпито большего всего рома`);
+
+console.log(maxMeatDay, maxRumDay);
